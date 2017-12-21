51b743d5de1adbb3aff7dc7ac2edef767bfa0e7e
Justine Cocchi <jucocchi@microsoft.com>
2017-12-21 11:41:17 -0800

    remove config file argument

--- src/arguments.ts
+++ src/arguments.ts
@@ -5,12 +5,6 @@ export default () => {
     const argsParser = new ArgumentParser(argParseOptions);
 
     argsParser.addArgument(
-        ["-c", "--config"], {
-            // TODO: fix this, used to be ../config.json but fails in typescript run bc .js is nested in extra dist folder
-            defaultValue: "../../config.json",
-            help: "Provide path to config.json file"
-        });
-    argsParser.addArgument(
         ["-r", "--restart"], {
             nargs: 0,
             help: "Restarts data transfer by deleting Cosmos DB collection and flushing Redis cache"
